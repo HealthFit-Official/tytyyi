963283e9cc10b2b5d1ce87e3a6e18054855b66c4
HealthFit.org <117991750+HealthFit-Official@users.noreply.github.com>
2023-05-21 13:34:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup() {
+    canvas= createCanvas(350, 350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350,350);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+   
+
+}
+
+function draw() {
+    image(video,0,0,350,350);
+    image(mustache,noseX-25, noseY+25, 60,40);
+}
+
+
+function modelLoaded()
+{    
+console.log('PoseNet Is Initialized');
+}
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
+    }
+
+}
+
+
+function take_snapshot() {
+    save('myFilteerImage.png');
+}
+
